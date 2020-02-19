b69f62dae00bd86fddb260bf05fc0e9d175f203f
Anis <anis_sg@hotmail.com>
2020-02-19 14:04:35 +0100

    test scene

--- Rambarde/Assets/Scripts/Hud.cs
+++ Rambarde/Assets/Scripts/Hud.cs
@@ -1,61 +1,53 @@
-﻿// using System.Collections.Generic;
-// using System.Linq;
-// using UniRx;
-// using Unity.UIElements.Runtime;
-// using UnityEngine;
-// using UnityEngine.UIElements;
-//
-// public class Hud : MonoBehaviour {
-//     [SerializeField] private Bard.Bard bard;
-//
-//     private PanelRenderer _panelRenderer;
-//     private UIElementsEventSystem _eventSystem;
-//
-//     private void Awake() {
-//         _panelRenderer = GetComponent<PanelRenderer>();
-//         _eventSystem = GetComponent<UIElementsEventSystem>();
-//     }
-//
-//     private void OnEnable() {
-//         _panelRenderer.postUxmlReload = BindUi;
-//     }
-//
-//     private IEnumerable<Object> BindUi() {
-//         var root = _panelRenderer.visualTree;
-//
-//         var bs = root.Q<VisualElement>("melodies");
-//
-//         bs.Children().Select(x => x as Button)
-//             .Where(x => x != null)
-//             .ToList()
-//             .ForEach(x => x.clickable.clicked += () => Debug.LogWarning("cannot select melody with old HUD") /*musicPlanner.PlaceMelody(int.Parse(x.name.Substring(1)))*/);
-//
-//         var reset = root.Q<Button>("reset");
-//         if (reset != null) {
-// <<<<<<< HEAD
-//             reset.clickable.clicked += bard.Reset;
-// =======
-//             //reset.clickable.clicked += _bard.Reset;
-//             Debug.Log(reset.name);
-// >>>>>>> develop-rhythmui
-//         }
-//
-//         var done = root.Q<Button>("done");
-//         if (done != null) {
-// <<<<<<< HEAD
-//             done.clickable.clicked += bard.Done;
-// =======
-//           //  done.clickable.clicked += _bard.Done;
-// >>>>>>> develop-rhythmui
-//         }
-//
-//         /*
-//         var energyText = root.Q<Label>("label");
-//         if (energyText != null) {
-//            musicPlanner.usedEnergy.AsObservable().Subscribe(x => energyText.text = x.ToString());
-//         }
-//         */
-//       
-//         return null;
-//     }
-// }
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using Unity.UIElements.Runtime;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Object = UnityEngine.Object;
+
+public class Hud : MonoBehaviour {
+    [SerializeField] private Bard _bard;
+
+    private PanelRenderer _panelRenderer;
+    private UIElementsEventSystem _eventSystem;
+
+    private void Awake() {
+        _panelRenderer = GetComponent<PanelRenderer>();
+        _eventSystem = GetComponent<UIElementsEventSystem>();
+    }
+
+    private void OnEnable() {
+        _panelRenderer.postUxmlReload = BindUi;
+    }
+
+    private IEnumerable<Object> BindUi() {
+        var root = _panelRenderer.visualTree;
+
+        var bs = root.Q<VisualElement>("melodies");
+
+        bs.Children().Select(x => x as Button)
+            .Where(x => x != null)
+            .ToList()
+            .ForEach(x => x.clickable.clicked += () => _bard.PlaceMelody(int.Parse(x.name.Substring(1))));
+
+        var reset = root.Q<Button>("reset");
+        if (reset != null) {
+            //reset.clickable.clicked += _bard.Reset;
+            Debug.Log(reset.name);
+        }
+
+        var done = root.Q<Button>("done");
+        if (done != null) {
+          //  done.clickable.clicked += _bard.Done;
+        }
+
+        var energyText = root.Q<Label>("label");
+        if (energyText != null) {
+            _bard.usedEnergy.AsObservable().Subscribe(x => energyText.text = x.ToString());
+        }
+        
+        return null;
+    }
+}
