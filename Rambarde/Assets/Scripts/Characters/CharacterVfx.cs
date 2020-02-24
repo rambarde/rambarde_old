f906482b7d1dbfea1787cc9c769ffb0ad383ca71
chooptr <michel.chaker@protonmail.com>
2020-02-24 10:11:40 +0100

    refactor health bar lerping

--- Rambarde/Assets/Scripts/Characters/CharacterVfx.cs
+++ Rambarde/Assets/Scripts/Characters/CharacterVfx.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
@@ -7,53 +8,49 @@ using UnityEngine.UI;
 namespace Characters {
     public class CharacterVfx : MonoBehaviour {
         [SerializeField] private Character character;
-        private TextMeshProUGUI _characterHealth;
-        
         [SerializeField] private GameObject greenBar;
-
         [SerializeField] private GameObject yellowBar;
-        
         [SerializeField] private GameObject redBar;
-        
+
+        private TextMeshProUGUI _characterHealth;
         private const float LerpTime = 1f;
-        
+
         private void Start() {
             Debug.Log(character.name);
             _characterHealth = GetComponentInChildren<TextMeshProUGUI>();
-            if (_characterHealth != null) {
-                character.stats.end.AsObservable().Subscribe(x => _characterHealth.text = x.ToString());
-                if(greenBar)
-                    character.stats.end.AsObservable().Pairwise().Subscribe(UpdateGreenBar);
+            if (_characterHealth == null) return;
+
+            character.stats.end.AsObservable().Subscribe(x => _characterHealth.text = x.ToString());
+
+            if (greenBar && yellowBar) {
+                character.stats.end.AsObservable().Pairwise().Subscribe(x =>
+                    Utils.UpdateGameObjectLerp(x, greenBar, 2, LerpTime, LerpHealthBar,
+                        pair => Utils.UpdateGameObjectLerp(x, yellowBar, 1, LerpTime, LerpHealthBar, _ => { })
+                    )
+                );
             }
         }
-        
-        private void UpdateYellowBar(Pair<float> values) {
-            float t = 0f, currentLerpTime = 0f;
-            yellowBar.LateUpdateAsObservable()
-                .TakeWhile(_ => currentLerpTime < LerpTime + Time.deltaTime)
-                .Subscribe(_ => {
-                    float f = Mathf.Lerp(values.Previous, values.Current, t / LerpTime);
-                    yellowBar.GetComponent<Image>().fillAmount = f / 100f;
-                    currentLerpTime += Time.deltaTime;
-                    t = currentLerpTime / LerpTime;
-                    t = Mathf.Sin(t * Mathf.PI * 0.5f);
-                });
+
+        private void LerpHealthBar(Pair<float> pair, GameObject go, ref float curLerpTime, float speed, float lerpTime, ref float t) {
+            float f = Mathf.Lerp(pair.Previous, pair.Current, t / LerpTime);
+            go.GetComponent<Image>().fillAmount = f / 100f;
+            curLerpTime += speed * Time.deltaTime;
+            t = curLerpTime / LerpTime;
+            t = Mathf.Sin(t * Mathf.PI * 0.5f);
         }
 
-        private void UpdateGreenBar(Pair<float> values) {
-            float t = 0f, currentLerpTime = 0f;
-            this.LateUpdateAsObservable()
-                .TakeWhile(_ => currentLerpTime < LerpTime + 2 * Time.deltaTime)
-                .DoOnCompleted(() => UpdateYellowBar(values))
-                .Subscribe(_ => {
-                    float f = Mathf.Lerp(values.Previous, values.Current, t / LerpTime);
-                    greenBar.GetComponent<Image>().fillAmount = f / 100f;
-                    currentLerpTime += 2 * Time.deltaTime;
-                    t = currentLerpTime / LerpTime;
-                    t = Mathf.Sin(t * Mathf.PI * 0.5f);
-                });
-        }
-
-
+        // private void UpdateBar(Pair<float> values, GameObject bar, int speed, Action<Pair<float>> update) {
+        //     float t = 0f, currentLerpTime = 0f;
+        //     bar.LateUpdateAsObservable()
+        //         .TakeWhile(_ => currentLerpTime < LerpTime + speed * Time.deltaTime)
+        //         .DoOnCompleted(() => update(values))
+        //         .Subscribe(_ => {
+        //             float f = Mathf.Lerp(values.Previous, values.Current, t / LerpTime);
+        //             bar.GetComponent<Image>().fillAmount = f / 100f;
+        //             currentLerpTime += speed * Time.deltaTime;
+        //             t = currentLerpTime / LerpTime;
+        //             t = Mathf.Sin(t * Mathf.PI * 0.5f);
+        //         });
+        // }
     }
 }
--- Rambarde/Assets/Scripts/Utils.cs
+++ Rambarde/Assets/Scripts/Utils.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Threading.Tasks;
 using UniRx;
+using UnityEngine;
 
 public static class Utils {
     public static async Task AwaitObservable<T>(IObservable<T> obs) {
         obs.Subscribe();
         await obs;
+    }
+
+    public delegate void LerpOnSubscribe(Pair<float> pair, GameObject go, ref float curLerpTime, float speed, float lerpTime, ref float t);
+    
+    public static void UpdateGameObjectLerp(Pair<float> values, GameObject go, float speed, float lerpTime, LerpOnSubscribe subscribe, Action<Pair<float>> doOnComplete) {
+        float t = 0f, currentLerpTime = 0f;
+        Observable.EveryLateUpdate()
+            .TakeWhile(_ => currentLerpTime < lerpTime + speed * Time.deltaTime)
+            .DoOnCompleted(() => doOnComplete(values))
+            .Subscribe(_ => { subscribe(values, go, ref currentLerpTime, speed, lerpTime, ref t); });
     }
 }
