49b26554e35230ba8b064e938400b6dd5452e226
amauryfauvel <amaury.fauvel@gmail.com>
2020-02-27 09:43:09 +0100

    add melody names in UI

--- Rambarde/Assets/Scripts/Hud.cs
+++ Rambarde/Assets/Scripts/Hud.cs
@@ -1,40 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Bard;
+using TMPro;
 using UniRx;
 using UniRx.Triggers;
-using Unity.UIElements.Runtime;
 using UnityEngine;
-using UnityEngine.Analytics;
 using UnityEngine.UI;
 
-public class Hud : MonoBehaviour
-{
+public class Hud : MonoBehaviour {
     public List<RectTransform> instPanels;
     public RectTransform actionPanel;
     public RectTransform inspiJauge;
 
-    public void Init(Bard.Bard bard)
-    {
+    public void Init(Bard.Bard bard) {
         GameObject buttonPrefab = Utils.LoadResourceFromDir<GameObject>("", "Button");
         GameObject separatorPrefab = Utils.LoadResourceFromDir<GameObject>("", "Separator");
 
-        for (int i = 0; i < bard.instruments.Count; ++i)
-        {
+        for (int i = 0; i < bard.instruments.Count; ++i) {
+            
             RectTransform panel = instPanels[i];
             Instrument inst = bard.instruments[i];
 
             int currentTier = 1;
-            foreach (var melody in inst.melodies)
-            {
-                if (melody.tier > currentTier)
-                {
+            foreach (var melody in inst.melodies) {
+                if (melody.tier > currentTier) {
                     Instantiate(separatorPrefab, panel);
                     currentTier = melody.tier;
                 }
-                var buttonGo = Instantiate(buttonPrefab, panel);
-                var button = buttonGo.GetComponent<Button>();
+                
+                GameObject buttonGo = Instantiate(buttonPrefab, panel);
+                Button button = buttonGo.GetComponent<Button>();
+                TextMeshProUGUI label = buttonGo.GetComponentInChildren<TextMeshProUGUI>();
+                label.text = melody.name;
                 button.OnClickAsObservable()
                     .Subscribe(_ => { bard.SelectMelody(melody); })
                     .AddTo(button);
@@ -46,22 +43,19 @@ public class Hud : MonoBehaviour
                     .AddTo(button);
             }
 
-            bard.actionPoints.Subscribe(p =>
-            {
+            bard.actionPoints.Subscribe(p => {
                 int nbrSquare = (bard.maxActionPoints.Value - p) / 8; //ATTENTION NOMBRE MAGIQUE NBR NOTE PER BEAT
                 int colored = 0;
                 List<Image> actionImages = actionPanel.GetComponentsInChildren<Image>().ToList();
                 actionImages.RemoveAt(0);
                 actionImages.Reverse();
-                foreach (Image image in actionImages)
-                {
+                foreach (Image image in actionImages) {
                     image.color = colored >= nbrSquare ? Color.white : Color.red;
                     colored++; 
                 }
             });
 
-            bard.inspiration.current.Subscribe(inspi =>
-            {
+            bard.inspiration.current.Subscribe(inspi => {
                 inspiJauge.localScale = new Vector3(1, inspi / (float) bard.inspiration.maximumValue, 1);
             });
         }
